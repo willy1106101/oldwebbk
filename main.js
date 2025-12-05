bc6ff3ec52f513ac008b1f11cb923cca10e7b68d
HSU WILLY <85432506+willy1106101@users.noreply.github.com>
2025-12-05 19:52:33 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+var mycars = new Array();
+//nav 
+//mycars["a0"] = "";//名稱
+//首頁編輯欄位
+//mycars['a數字-1'] = "";//網址
+mycars["a0"] = "首頁";
+mycars['a0-1'] = "index.html";
+mycars["a1"] = "";
+mycars['a1-1'] = "";
+mycars["a2"] = "";
+mycars['a2-1'] = "";
+mycars["a3"] = "";
+mycars['a3-1'] = "";
+//font-left
+//mycars["b0"] = "";
+mycars["b0"] = "";
+//font-right
+//mycars["c0"] = "";
+mycars["c0"] = "";
+
